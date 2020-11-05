925f064d9743b9acfaac6d2100af21884f7268c6
Ben <benjamin.jenkins@gocompare.com>
2020-11-05 16:42:03 +0000

    Loads data from local storage

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const container = document.querySelector('.container');
-const seats = document.queryCommandEnabled('.row .seat:not(.occupied)');
-
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const filmSelect = document.getElementById('film');
+
+populateUI();
+
 let ticketPrice = +filmSelect.value;
 
 container.addEventListener('click', (e) => {
@@ -15,25 +17,41 @@ container.addEventListener('click', (e) => {
 
 filmSelect.addEventListener('change', e => {
     ticketPrice = e.target.value;
-    setFilmData();
+    setFilmData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
-function setFilmData(filmIndex, filmPrice){
+function setFilmData(filmIndex, filmPrice) {
     localStorage.setItem('selectedFilmIndex', filmIndex);
     localStorage.setItem('selectedFilmPrice', filmPrice);
 }
 
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    const selectedSeatsCount = selectedSeats.length;
-
-    const seatsIndex = [...selectedSeats].map((seat) =>
-        [...seats].indexOf(seat)
-    );
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
 
     localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+    const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
 }
+
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedFilmIndex = localStorage.getItem('selectedFilmIndex');
+    if (selectedFilmIndex != null) {
+        filmSelect.selectedIndex = selectedFilmIndex;
+    }
+}
+
+updateSelectedCount();
